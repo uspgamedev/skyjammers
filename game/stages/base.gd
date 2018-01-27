adf0e9dd3b9e2a06b19e6476b7a6dbc1628654b3
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2018-01-27 03:02:51 -0200

    Added sentinel

--- game/stages/base.gd
+++ game/stages/base.gd
@@ -4,6 +4,7 @@ export(NodePath) var initial_bard_path
 
 onready var activeInstrument = null
 onready var camera = $Camera2D
+var game_over = false
 
 func _ready():
 	activeInstrument = get_node(initial_bard_path)
@@ -12,9 +13,12 @@ func _ready():
 
 func changeActive(body):
 	self.activeInstrument = body
+	if activeInstrument.is_in_group("sentinels"):
+		game_over = true
+		OS.alert("GAME OVER", "u suk")
 
 func _on_Input_played():
-	if not activeInstrument.hasSoundwave:
+	if not game_over and not activeInstrument.hasSoundwave:
 			activeInstrument.pulse()
 
 func _process(delta):
