01c1ae316444963a1715e4fca9857226fd9898fe
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2018-01-27 18:09:39 -0200

    Added star shape for GameBoy

---
+++ game/Instruments/GameBoy/GameBoyWaveShape.gd
@@ -0,0 +1,31 @@
+extends CollisionShape2D
+
+var is_active = false
+var fix_size = 2
+var ratio = .38
+var size = 5
+
+func _ready():
+	pass
+
+func _process(delta):
+	if is_active:
+		update()
+
+func draw_star(radius, alpha):
+	var aux
+	var outer = Vector2(0, -radius)
+	var inner = (outer * ratio).rotated(PI/size)
+	
+	for i in range (1, size+1):
+		draw_line(outer, inner, Color(0,0,0,alpha))
+		outer = outer.rotated(2*PI/size)
+		draw_line(inner, outer, Color(0,0,0,alpha))
+		inner = inner.rotated(2*PI/size)
+	
+func _draw():
+	draw_star(10, .9)
+	draw_star(8, .8)
+	draw_star(6, .7)
+	draw_star(4, .6)
+	draw_star(2, .5)
