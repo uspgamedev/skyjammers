b3728a1be9f40f162b6a859cd2d8378d35530922
rilifon <pudimdefrutas@gmail.com>
2018-01-27 14:15:46 -0200

    Added visuals for paths

--- game/Movement/path.gd
+++ game/Movement/path.gd
@@ -2,3 +2,12 @@ extends Path2D
 
 export (bool) var cyclic
 export (float) var speed = 1
+
+onready var points = get_curve().get_baked_points()
+
+func _process(delta):
+	update()
+
+func _draw():
+	for i in range(0,points.size()-1, 4):
+		draw_line(points[i],points[i+1],Color(.2,.2,.6,.7), 5)
