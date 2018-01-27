21f96b3b744b17bee5fa6f6f2e5e65003d991ae2
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2018-01-27 02:43:54 -0200

    Added SoundWaveShape object to Instrument

--- game/Instruments/Instrument.gd
+++ game/Instruments/Instrument.gd
@@ -6,8 +6,12 @@ var stage
 
 func pulse():
 	var sw = soundWave.instance()
+	var shape = get_node("SoundWaveShape").duplicate()
 	
 	sw.set_position(get_global_position())
+	sw.set_rotation(get_parent().get_rotation())
 	sw.instrument = self
 	sw.stage = self.stage
+	shape.show()
+	sw.add_child(shape)
 	stage.get_node("Instruments").add_child(sw)
