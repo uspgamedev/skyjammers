c623e297ee91860454f0c142ecf9e7e07170f6ea
Victor Domiciano <victor.dm3@gmail.com>
2018-01-27 02:19:04 -0200

    Fixed soundwave position

--- game/Instruments/Instrument.gd
+++ game/Instruments/Instrument.gd
@@ -6,7 +6,7 @@ var stage
 
 func pulse():
 	var sw = soundWave.instance()
-	sw.set_position(get_position())
+	sw.set_position(get_global_position())
 	sw.instrument = self
 	sw.stage = self.stage
 	stage.get_node("Instruments").add_child(sw)
