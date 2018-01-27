c8f0419cf3e4925c1c5888dcbb9a3a9e288734fb
Kazuo256 <kazuo256@gmail.com>
2018-01-27 03:18:32 -0200

    I don't even

--- game/SoundWave/SoundWave.gd
+++ game/SoundWave/SoundWave.gd
@@ -3,6 +3,8 @@ extends Area2D
 var instrument
 var stage
 
+export(float) var duration = 2
+export(float) var growth = 5
 
 func _ready():
 	pulse()
@@ -12,7 +14,8 @@ func pulse():
 	var tween = get_node("Tween")
 	
 	instrument.hasSoundwave = true
-	tween.interpolate_property(self, "scale", get_scale(), get_scale() * 30, 5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	tween.interpolate_property(self, "scale", get_scale(), get_scale() * self.growth,
+							   self.duration, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	tween.start()
 	yield(tween, "tween_completed")
 	selfDestruct()
