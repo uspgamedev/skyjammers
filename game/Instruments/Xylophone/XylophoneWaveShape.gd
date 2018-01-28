4cb900f0552b853db3e0e503fa0b922131ec7f96
Detril <andremouka@hotmail.com>
2018-01-28 13:08:23 -0200

    Made soundwave drawings less horrible

--- game/Instruments/Xylophone/XylophoneWaveShape.gd
+++ game/Instruments/Xylophone/XylophoneWaveShape.gd
@@ -14,7 +14,7 @@ func _process(delta):
 
 func _draw():
 	draw_rect(Rect2(fix_size*Vector2(-10,-5), fix_size*Vector2(20,10)), cor, false)
-	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.8, fix_size*Vector2(20,10)*.8), cor, false)
-	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.6, fix_size*Vector2(20,10)*.6), cor, false)
-	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.4, fix_size*Vector2(20,10)*.4), cor, false)
-	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.2, fix_size*Vector2(20,10)*.2), cor, false)
+#	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.8, fix_size*Vector2(20,10)*.8), cor, false)
+#	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.6, fix_size*Vector2(20,10)*.6), cor, false)
+#	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.4, fix_size*Vector2(20,10)*.4), cor, false)
+#	draw_rect(Rect2(fix_size*Vector2(-10,-5)*.2, fix_size*Vector2(20,10)*.2), cor, false)
