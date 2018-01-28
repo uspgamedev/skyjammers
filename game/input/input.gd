1555e04fe0ec38331a277d14e320b20b408f702d
rilifon <pudimdefrutas@gmail.com>
2018-01-28 01:50:17 -0200

    Grayscale shader ayyyyy

--- game/input/input.gd
+++ game/input/input.gd
@@ -10,6 +10,7 @@ func _unhandled_input(event):
 	if event.is_action_pressed("action_play"):
 		if stage.lose:
 			get_tree().set_pause(false)
+			get_parent().get_node("CanvasLayer2/grayscaleShader").material.set_shader_param("grayscale", false)
 			get_tree().reload_current_scene()
 		else:
 			emit_signal("played")
