fba5969ea1c39a3aec093a553f0220cefa9c33ed
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2018-01-28 04:03:57 -0200

    Stage selector gradient

--- game/menus/stage-selector/StageSelector.gd
+++ game/menus/stage-selector/StageSelector.gd
@@ -2,6 +2,11 @@ extends Control
 
 onready var audioManager = get_node("AudioManager")
 
+func _set_offset(y):
+	self.rect_position.y = y
+	var h = get_viewport_rect().size.y #OS.window_size.y
+	$Panel.material.set_shader_param("offset", y/h)
+
 func _ready():
 	audioManager.playBGM("LevelSelect")
 	var total = get_tree().get_root().get_node("Progress").get_total() + 1
@@ -12,6 +17,6 @@ func _ready():
 		else:
 			button.hide()
 		count += 1
-	$FadeInTween.interpolate_property(self, "rect_position", Vector2(0, -1000), Vector2(0,0), 1,
+	$FadeInTween.interpolate_method(self, "_set_offset", -1000, 0, 1,
 									  Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$FadeInTween.start()
