961c26916f5bc10312fa5a992198a6036c4c0746
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2018-01-28 02:48:31 -0200

    Fix mouse click

--- game/input/input.gd
+++ game/input/input.gd
@@ -4,8 +4,8 @@ signal played
 onready var stage = get_parent()
 
 func _ready():
-	set_process_unhandled_input(true)
+	set_process_input(true)
 
-func _unhandled_input(event):
+func _input(event):
 	if event.is_action_pressed("action_play"):
 		emit_signal("played")
